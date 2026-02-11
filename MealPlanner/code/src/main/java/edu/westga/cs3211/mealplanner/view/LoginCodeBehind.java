bf7d46b08093185679e3731757b5ea33816e39c3
JarethUWG <jbatty1@my.westga.edu>
2026-02-10 22:55:25 -0500

    Created login view
    Created a view for login and a basic project.

---
+++ MealPlanner/code/src/main/java/edu/westga/cs3211/mealplanner/view/LoginCodeBehind.java
@@ -0,0 +1,41 @@
+package edu.westga.cs3211.mealplanner.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginCodeBehind {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private AnchorPane loginPane;
+
+    @FXML
+    private Label passwordReminderText;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Label usernameReminderText;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    void createAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+
+    }
+
+}
+
