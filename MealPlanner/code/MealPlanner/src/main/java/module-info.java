5645bade21ae205bca548fb25016d9f883b65a08
William  Vanderboom <wv00005@my.westga.edu>
2026-02-11 20:54:49 -0500

    Reverted the first pull request to remove uneeded files. Revert "Merge pull request #3 from JarethUWG/SetupMavenProject"
    This reverts commit 4ec42660898b3326b14470ebb21d17659b32c73d, reversing
    changes made to 64bc6bdc1a0f0407d021a4b913bfb45cb65f4c8a.

--- MealPlanner/code/MealPlanner/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module edu.westga.cs3212.mealplanner {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
-    requires javafx.base;
-
-    opens edu.westga.cs3212.mealplanner.view to javafx.fxml;
-    exports edu.westga.cs3212.mealplanner;
-}
