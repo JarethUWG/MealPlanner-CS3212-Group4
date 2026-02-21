8a6c264095c8d6fc10bf710d9e90f7b56db95123
CBaesman <cbaesma1@my.westga.edu>
2026-02-21 03:05:00 -0500

    Changed ingredient implementation to match agreed system, added testing for ingredient

---
+++ MealPlanner/code/src/main/java/edu/westga/cs3212/mealplanner/model/Ingredient.java
@@ -0,0 +1,45 @@
+package edu.westga.cs3212.mealplanner.model;
+
+/**
+ * Basic class for Ingredients.
+ */
+public class Ingredient {
+    private String name;
+    private double calories;
+
+    /**
+     * Basic constructor for an Ingredient.
+     * @param name Name of the ingredient
+     * @param calories Caloric value of the ingredient
+     *
+     * @post this.name = name, this.calories = calories, this.desc = desc.
+     * If name is not provided, defaults to "Unnamed Ingredient".
+     * If calories given are below 0, defaults to 0.
+     */
+    public Ingredient(String name, double calories) {
+        this.name = name;
+        this.calories = calories;
+        if (name == null || name.isBlank()) {
+            this.name = "Unnamed Ingredient";
+        }
+        if (calories < 0) {
+            this.calories = 0;
+        }
+    }
+
+    /**
+     * Retrieves the ingredient name.
+     * @return The name of the ingredient.
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Retrieves the individual ingredient calories.
+     * @return Caloric value of the ingredient.
+     */
+    public double getCalories() {
+        return this.calories;
+    }
+}
