c28352c65d6ff9cb7cd8b5b8c37f2dfe6da3d53e
CBaesman <cbaesma1@my.westga.edu>
2026-03-04 19:10:37 -0500

    Emergency missing import

--- MealPlanner/code/src/main/java/edu/westga/cs3212/mealplanner/view/PlannerCodeBehind.java
+++ MealPlanner/code/src/main/java/edu/westga/cs3212/mealplanner/view/PlannerCodeBehind.java
@@ -1,5 +1,6 @@
 package edu.westga.cs3212.mealplanner.view;
 
+import edu.westga.cs3212.mealplanner.viewmodel.PlannerViewModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
